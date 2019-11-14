33bbd775a1e2be2636da692959798b317afb657d
Kenny Bastani <kb@socialmoon.com>
2019-11-14 15:59:24 +0100

    Various fixes for service discovery and docker compose example

--- vlingo-xoom-examples/advanced-microservice/discovery-service/src/test/java/com/example/demo/DemoApplicationTests.java
+++ vlingo-xoom-examples/advanced-microservice/discovery-service/src/test/java/com/example/demo/DemoApplicationTests.java
@@ -1,9 +1,10 @@
 package com.example.demo;
 
+import io.examples.demo.DemoApplication;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(classes = DemoApplication.class)
 class DemoApplicationTests {
 
 	@Test
