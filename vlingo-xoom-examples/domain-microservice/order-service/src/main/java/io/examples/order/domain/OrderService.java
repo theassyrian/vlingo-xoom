28eb1a9ecd4a90610642226e1ffbd7689b297ee6
Kenny Bastani <kb@socialmoon.com>
2019-12-05 17:01:36 -0500

    Describing the order workflow

--- vlingo-xoom-examples/domain-microservice/order-service/src/main/java/io/examples/order/domain/OrderService.java
+++ vlingo-xoom-examples/domain-microservice/order-service/src/main/java/io/examples/order/domain/OrderService.java
@@ -4,6 +4,7 @@ import io.examples.order.domain.state.OrderStatus;
 import io.examples.order.domain.state.processor.ProcessorService;
 import io.examples.order.infra.repository.OrderRepository;
 import io.vlingo.common.Completes;
+import io.vlingo.xoom.processor.Processor;
 
 import javax.inject.Provider;
 import javax.inject.Singleton;
@@ -19,13 +20,18 @@ public class OrderService {
         this.processorService = processorService.get();
     }
 
+    /**
+     * Here we define an {@link Order} and begin the order workflow process by moving along the state of the
+     * order from ORDER_CREATED to RESERVATION_PENDING.
+     *
+     * @param orderDefinition is the definition of the order that we will be creating
+     * @return a completes publisher that will execute as the response is returned to the HTTP resource consumer
+     */
     public Completes<Order> defineOrder(Order orderDefinition) {
-        // Remove the identity from the shipping address and save the order
+        final Processor processor = processorService.getProcessor();
         return Completes.withSuccess(orderDefinition)
-                .andThen(order -> order.sendEvent(processorService.getProcessor(),
-                        OrderStatus.ACCOUNT_CONNECTED))
-                .andThen(order -> order.sendEvent(processorService.getProcessor(),
-                        OrderStatus.RESERVATION_PENDING))
+                .andThen(order -> order.sendEvent(processor, OrderStatus.ACCOUNT_CONNECTED))
+                .andThen(order -> order.sendEvent(processor, OrderStatus.RESERVATION_PENDING))
                 .andThenConsume(orderRepository::save)
                 .andThenConsume(order -> queryOrder(order.getId()))
                 .otherwise(order -> {
